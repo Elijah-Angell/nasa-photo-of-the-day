b8750e209779779e32620b907c9c1705ca8d806d
user <useruser@Guests-MacBook-Pro.local>
2021-07-14 19:15:21 -0400

    useEffect put up with axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios'
+
+
+
+
+
+
+
+
+
 
 function App() {
+  const [nasapic, setNasaPic] = useState('');
+
+  useEffect(() => {
+    axios.get(" https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1")
+      .then(res => {
+        setNasaPic(res.data)
+        console.log(res.data)
+      })
+      .catch(err => console.log(err))
+  }, []);
+
+
+
+
+
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <img src={nasapic} alt="a random space pic" />
     </div>
   );
 }
